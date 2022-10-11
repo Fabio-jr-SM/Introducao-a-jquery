ef91fbc6adde781d0434bb880ad8821d297682ed
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-10-11 10:19:50 -0400

    Add files via upload

---
+++ Introdução a JQUERY - Parte 2/js/full.js
@@ -0,0 +1,46 @@
+$("#fadeIn").click(function(){
+    $("div").fadeIn()
+})
+
+$("#fadeOut").click(function(){
+    $("div").fadeOut()
+})
+
+
+$("#fadeToggle").click(function(){
+    $("div").fadeToggle()
+})
+
+$("#fadeTo5").click(function(){
+    $("div").fadeTo("fast", 0.5)
+})
+
+$("#fadeTo1").click(function(){
+    $("div").fadeTo1("fast", 1.0)
+})
+
+$("#hide").click(function(){
+    $("div").hide()
+})
+
+$("#show").click(function(){
+    $("div").show()
+})
+
+$("#toggle").click(function(){
+    $("div").toggle()
+})
+
+
+$("#SlideDown").click(function(){
+    $("div").slideDown()
+})
+
+$("#slideUp").click(function(){
+    $("div").slideUp()
+})
+
+$("#SlideToggle").click(function(){
+    $("div").slideToggle()
+})
+
---
+++ Introdução a JQUERY - Parte 2/js/jqSlideUpAndDown.js
@@ -0,0 +1,7 @@
+$("#esconder").click(function(){
+    $("div").slideUp("slow")
+})
+
+$("#mostrar").click(function(){
+    $("div").slideDown("fast")
+})
